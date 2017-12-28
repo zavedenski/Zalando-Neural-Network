c061be538c76ea81dcb9bbf5bef568e7f5050ec9
Vladyslav Zavedenski <zavedenski@gmail.com>
2017-12-29 01:24:45 +0200

    Make some changes with main values like weigth, learning rate and so on. Add file with performance measurement with different parameters.

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import scipy.special
 import numpy
+import time
 import csv
 
 #======Class-Space==============================================================
@@ -58,80 +59,87 @@ class neuralNetwork:
 
 
 #---Initialization-of-class-object----------------------------------------------
-input_nodes = 784       #input_nodes equel: 28 x 28 pixels
-hidden_nodes = 10      #hidden_nodes equel count of synapses between neurons
-output_nodes = 10       #output_nodes equel: 10 number [0 - 9]
+input_nodes = 784
+hidden_nodes = int(input("Please enter count of hidden nodes: "))
+output_nodes = 10
 
-learning_rate = 0.1
+learning_rate = float(input("Please enter learning rate: "))
+epochs = int(input("Please enter count of epochs: "))
 
-N = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
-#---Initialization-of-class-object-END------------------------------------------
+loop = 0
+while loop <= 20:
+    time0 = time.time()
+    N = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+    #---Initialization-of-class-object-END------------------------------------------
 
-#---Training-neural-network-by-mnist_train-file---------------------------------
-training_data_file = open('MnistDataset/mnist_train.csv', 'r')
-training_data_list = training_data_file.readlines()
-training_data_file.close()
+    #---Training-neural-network-by-mnist_train-file---------------------------------
+    training_data_file = open('MnistDataset/mnist_train.csv', 'r')
+    training_data_list = training_data_file.readlines()
+    training_data_file.close()
 
-epochs = 1
-for e in range(epochs):
-    for record in training_data_list:
+    for e in range(epochs):
+        for record in training_data_list:
+            all_values = record.split(',')
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            targets = numpy.zeros(output_nodes) + 0.01
+            targets[int(all_values[0])] = 0.99
+            N.train(inputs, targets)
+            pass
+        pass
+    #---Training-neural-network-by-mnist_train-file-END-----------------------------
+
+    #---Testing-neural-network-by-mnist_test-file-----------------------------------
+    test_data_file = open('MnistDataset/mnist_test.csv', 'r')
+    test_data_list = test_data_file.readlines()
+    test_data_file.close()
+
+    all_values = test_data_list[0].split(',')
+    #print('[', training_data_list[0], '] Training data')
+
+    image_array = numpy.asfarray(all_values[1:]).reshape((28,28))
+    plt.imshow(image_array, cmap='Greys', interpolation='None')
+
+    N.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01)
+
+    scoreCard = []
+
+    for record in test_data_list:
         all_values = record.split(',')
+        correct_label = int(all_values[0])
+
         inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        targets = numpy.zeros(output_nodes) + 0.01
-        targets[int(all_values[0])] = 0.99
-        N.train(inputs, targets)
+        outputs = N.query(inputs)
+        label = numpy.argmax(outputs)
+        #print('Correct[{c}] ===> [{l}]Selected'.format(c = correct_label, l = label))
+
+        if (label == correct_label):
+            scoreCard.append(1)
+        else:
+            scoreCard.append(0)
+            pass
         pass
-    pass
-#---Training-neural-network-by-mnist_train-file-END-----------------------------
+    timeDiff = time.time() - time0
+    #---Testing-neural-network-by-mnist_test-file-END-------------------------------
 
-#---Testing-neural-network-by-mnist_test-file-----------------------------------
-test_data_file = open('MnistDataset/mnist_test.csv', 'r')
-test_data_list = test_data_file.readlines()
-test_data_file.close()
+    #===Convert-Data-And-Put-Into-File==============================================
+    scoreCardArray = numpy.asarray(scoreCard)
+    performance = scoreCardArray.sum() / scoreCardArray.size
+    print("\nPerfomance equel: [", (performance * 100), '] percents!')
 
-all_values = test_data_list[0].split(',')
-print('[', training_data_list[0], '] Training data')
+    performance_csv = []
+    performance_csv.append(performance)
+    performance_csv.append(learning_rate)
+    performance_csv.append(hidden_nodes)
+    performance_csv.append(epochs)
+    performance_csv.append(timeDiff)
+    performance_csv.append(';  ')
+    print('Perfomance, learnRate, hNodes, epochs, timeDiff')
+    print(performance_csv)
 
-image_array = numpy.asfarray(all_values[1:]).reshape((28,28))
-plt.imshow(image_array, cmap='Greys', interpolation='None')
-
-N.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01)
-
-scoreCard = []
-
-for record in test_data_list:
-    all_values = record.split(',')
-    correct_label = int(all_values[0])
-
-    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    outputs = N.query(inputs)
-    label = numpy.argmax(outputs)
-    print('Correct[{c}] ===> [{l}]Selected'.format(c = correct_label, l = label))
-
-    if (label == correct_label):
-        scoreCard.append(1)
-    else:
-        scoreCard.append(0)
-        pass
-    pass
-#---Testing-neural-network-by-mnist_test-file-END-------------------------------
-
-#===Convert-Data-And-Put-Into-File==============================================
-scoreCardArray = numpy.asarray(scoreCard)
-performance = scoreCardArray.sum() / scoreCardArray.size
-print("Perfomance equel: [", (performance * 100), '] percents!')
-
-performance_csv = []
-performance_csv.append(performance)
-performance_csv.append(learning_rate)
-performance_csv.append(hidden_nodes)
-performance_csv.append(epochs)
-performance_csv.append(';  ')
-print('Perfomance, learnRate, hNodes, epochs')
-print(performance_csv)
-
-csv_file = open('performance_history.csv', 'a')
-wr = csv.writer(csv_file)
-wr.writerows([performance_csv])
-csv_file.close()
-#===Convert-Data-And-Put-Into-File=END==========================================
+    csv_file = open('performance_history.csv', 'a')
+    wr = csv.writer(csv_file)
+    wr.writerows([performance_csv])
+    csv_file.close()
+    #===Convert-Data-And-Put-Into-File=END==========================================
+    learning_rate += 0.001
+    loop += 1
